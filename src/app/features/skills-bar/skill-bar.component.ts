00489bf7e4214c93a71574e388ac2c0e71ce53f5
Jakub Lasek <jakublasek41@gmail.com>
2024-06-02 21:57:52 +0200

    Prepare mat-tree

--- src/app/features/skills-bar/skill-bar.component.ts
+++ src/app/features/skills-bar/skill-bar.component.ts
@@ -1,11 +1,69 @@
+import { FlatTreeControl } from "@angular/cdk/tree";
 import { NgOptimizedImage } from "@angular/common";
 import { Component } from "@angular/core";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { MatTreeFlatDataSource, MatTreeFlattener, MatTreeModule } from "@angular/material/tree";
+
+interface FoodNode {
+  name: string;
+  children?: FoodNode[];
+}
+
+const TREE_DATA: FoodNode[] = [
+  {
+    name: 'Character',
+    children: [{ name: 'Equipment' }, { name: 'Bank' }],
+  },
+  {
+    name: 'Fighting',
+    children: [
+      {
+        name: 'Strength',
+      },
+      {
+        name: 'Defence',
+      },
+    ],
+  },
+];
+
+interface ExampleFlatNode {
+  expandable: boolean;
+  name: string;
+  level: number;
+}
 
 @Component({
   selector: "skill-bar",
   templateUrl: "./skill-bar.component.html",
   styleUrl: "./skill-bar.component.scss",
   standalone: true,
-  imports: [NgOptimizedImage]
+  imports: [NgOptimizedImage, MatTreeModule, MatButtonModule, MatIconModule]
 })
-export class SkillBarComponent { }
+export class SkillBarComponent {
+  private _transformer = (node: FoodNode, level: number): ExampleFlatNode => {
+    return {
+      expandable: !!node.children && node.children.length > 0,
+      name: node.name,
+      level
+    }
+  }
+
+  treeControl = new FlatTreeControl<ExampleFlatNode>(
+    node => node.level,
+    node => node.expandable
+  )
+
+  treeFlattener = new MatTreeFlattener(
+    this._transformer,
+    node => node.level,
+    node => node.expandable,
+    node => node.children
+  )
+
+  dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener, TREE_DATA);
+
+  hasChild = (_: number, node: ExampleFlatNode) => node.expandable;
+
+}
--- src/app/ui/templates/game-view/game-view.component.ts
+++ src/app/ui/templates/game-view/game-view.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { MatGridListModule } from '@angular/material/grid-list';
 
 @Component({
   selector: 'ui-game-view-template',
   standalone: true,
-  imports: [MatGridListModule],
+  imports: [],
   styleUrl: "./game-view.component.scss",
   templateUrl: './game-view.component.html',
 })
