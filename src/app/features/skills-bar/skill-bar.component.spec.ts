76a0c1d3de895344e7a5e20aa9a6da87dd6bc44e
Jakub Lasek <jakublasek41@gmail.com>
2024-06-14 18:48:01 +0200

    Prepare proper test configuration

---
+++ src/app/features/skills-bar/skill-bar.component.spec.ts
@@ -0,0 +1,13 @@
+import { SkillBarComponent } from "./skill-bar.component";
+
+describe('SkillBarComponent', () => {
+  let component: SkillBarComponent;
+
+  beforeEach(() => {
+    component = new SkillBarComponent();
+  })
+
+  it('should create component', () => {
+    expect(component).toBeTruthy();
+  })
+})
