460edbbc2b4763164d9c6209d27a9198f5ca4c2e
Jakub Lasek <jakublasek41@gmail.com>
2024-06-18 18:53:56 +0200

    Prepare styling for app-bar

--- src/app/features/app-bar/ui/calendar/calendar.component.ts
+++ src/app/features/app-bar/ui/calendar/calendar.component.ts
@@ -1,10 +1,15 @@
-import { Component } from "@angular/core";
+import { DatePipe } from "@angular/common";
+import { Component, InputSignal, input } from "@angular/core";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
 
 @Component({
   selector: 'calendar',
   templateUrl: './calendar.component.html',
   styleUrl: './calendar.component.scss',
   standalone: true,
-  imports: []
+  imports: [MatIconModule, MatButtonModule, DatePipe]
 })
-export class CalendarComponent { }
+export class CalendarComponent {
+  gameDate: InputSignal<Date> = input<Date>(new Date());
+}
